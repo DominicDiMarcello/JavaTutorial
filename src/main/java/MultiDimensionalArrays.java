e5505387954406b222c513b3d99e8634202d4c4c
DominicDiMarcello <103143836+DominicDiMarcello@users.noreply.github.com>
2022-04-20 09:47:16 -0400

    Edits

--- src/main/java/MultiDimensionalArrays.java
+++ src/main/java/MultiDimensionalArrays.java
@@ -4,7 +4,6 @@ public class MultiDimensionalArrays {
 
     public static void main(String[] args) {
         int [][] numbers = { {1, 2, 3}, {4, 5, 6}};
-        numbers[0][0] = 1;
         System.out.println(Arrays.deepToString(numbers));
     }
 
